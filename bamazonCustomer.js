88a88a41c72a5afda234646ad8b73abb2c9d1d9e
SophieM <47410186+SophM@users.noreply.github.com>
2019-03-24 16:03:35 -0700

    add updated bamazonCustomer.js - add validation for first question, a warning message is displayed if the customer enters a letter other than q or a number that is not a current id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,13 +23,13 @@ var table = require("cli-table");
 // create the connection to bamazon_db and store it in a variable
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // my port
     port: 3306,
-  
+
     // my username
     user: "root",
-  
+
     // my password
     password: "a9uLc2frfg7buEidFA",
     database: "bamazon_db"
@@ -43,9 +43,10 @@ var connection = mysql.createConnection({
 // create a color to display the "goodbye" message with cli-color
 var goodbyeMsg = clc.xterm(208);
 
-// create a color to display the "transaction" messages - too low quantity or successful transaction
+// create a color to display "warning" messages when too low quantity or not valid id
+// and "successful-transaction" message
 var sucessMsg = clc.xterm(71);
-var tooLowMsg = clc.xterm(160);
+var warningMsg = clc.xterm(160);
 
 
 // ------------------------------------------------------------------
@@ -58,7 +59,7 @@ function showInventory(arr) {
     var productsTable = new table();
     // add the first row = headers - with some styling thanks to cli-color
     productsTable.push([clc.cyan.bold("item_id"), clc.cyan.bold("product_name"), clc.cyan.bold("department_name"), clc.cyan.bold("price"), clc.cyan.bold("stock_quantity")]);
-    // add value to the table
+    // add every item to the table
     for (var i = 0; i < arr.length; i++) {
         productsTable.push(
             [arr[i].item_id, arr[i].product_name, arr[i].department_name, arr[i].price, arr[i].stock_quantity],
@@ -69,19 +70,20 @@ function showInventory(arr) {
 }
 
 // function to diplay the "products" table to the cutomer = our inventory
-// and ask if she/he want to buy something
+// and ask if she/he wants to buy something
 function showInventoryAskCustomer() {
     // connect to the database to do a request
     connection.query(
         // select the entire "products" table
         "SELECT * FROM products",
+        // then run the following
         function (err, res) {
             // if error(s), display it(them)
             if (err) throw err;
             // show the "products" table in a nice table
             showInventory(res);
             // ask the customer if she/he wants to buy something
-            askCustomer();  
+            askCustomer();
         }
     )
 }
@@ -96,65 +98,93 @@ function askCustomer() {
                 message: "Which item would you like to purchase ? Enter its ID [Quit with Q]",
                 name: "customerChoice"
             }
-        ]).then (function(item) {
-            // if the customer entered "Q"
-            if(item.customerChoice.toLowerCase() === "q") {
-                // display a "goodbye" message
-                console.log(goodbyeMsg.bold("-----------------------------------"));
-                console.log(goodbyeMsg.bold("Goodbye, see you next time!"));
-                console.log(goodbyeMsg.bold("-----------------------------------"));
-                // end the connection with bamazon_db
-                connection.end();
-            // if the customer enter an ID
-            } else {
-                // ask for how many she/he wants
-                inquirer
-                    .prompt([
-                        {
-                            type: "input",
-                            message: "How many would you like to buy? [Quit with Q]",
-                            name: "customerQuantity"
-                        }
-                    ]).then(function(quantity) {
-                        // if the customer entered "Q"
-                        if(quantity.customerQuantity.toLowerCase() === "q") {
-                            // display a "goodbye" message
-                            console.log(goodbyeMsg.bold("-----------------------------------"));
-                            console.log(goodbyeMsg.bold("Goodbye, see you next time!"));
-                            console.log(goodbyeMsg.bold("-----------------------------------"));
-                            // end the connection with bamazon_db
-                            connection.end();
-                        // if the customer entered a quantity
-                        } else {
-                            // run updateInventory() function
-                            updateInventory(item.customerChoice, quantity.customerQuantity);
-                        }
-                    })              
-            }
+        // then run the following
+        ]).then(function (item) {
+            // connect to the database to do a request
+            connection.query(
+                // select the column "item_id" from "products" table
+                "SELECT item_id FROM products",
+                // then run the following
+                function (err, res) {
+                    // if error(s), display it(them)
+                    if (err) throw err;
+                    // store all the current ids in an array
+                    var currentId = [];
+                    for (var i = 0; i < res.length; i ++) {
+                        currentId.push(res[i].item_id);
+                    }
+                    // console.log(currentId);
+                    // if the number entered by the customer is not a valid ID or is a letter other than "q"
+                    if (!currentId.includes(parseInt(item.customerChoice)) && item.customerChoice.toLowerCase() !== "q") {
+                        // display message - not valid ID
+                        console.log(warningMsg.bold("-----------------------------------"));
+                        console.log(warningMsg.bold("This ID is not recognized. Please enter a valid ID!"));
+                        console.log(warningMsg.bold("-----------------------------------"));
+                        // ask the customer again if she/he wants to buy something
+                        askCustomer();
+                    // if the customer entered "Q" or "q"
+                    } else if (item.customerChoice.toLowerCase() === "q") {
+                        // display a "goodbye" message
+                        console.log(goodbyeMsg.bold("-----------------------------------"));
+                        console.log(goodbyeMsg.bold("Goodbye, see you next time!"));
+                        console.log(goodbyeMsg.bold("-----------------------------------"));
+                        // end the connection with bamazon_db
+                        connection.end();
+                    // if the customer enter a valid ID
+                    } else {
+                        // ask for how many she/he wants
+                        inquirer
+                            .prompt([
+                                {
+                                    type: "input",
+                                    message: "How many would you like to buy? [Quit with Q]",
+                                    name: "customerQuantity"
+                                }
+                            // then run the following
+                            ]).then(function (quantity) {
+                                // if the customer entered "Q" or "q"
+                                if (quantity.customerQuantity.toLowerCase() === "q") {
+                                    // display a "goodbye" message
+                                    console.log(goodbyeMsg.bold("-----------------------------------"));
+                                    console.log(goodbyeMsg.bold("Goodbye, see you next time!"));
+                                    console.log(goodbyeMsg.bold("-----------------------------------"));
+                                    // end the connection with bamazon_db
+                                    connection.end();
+                                // if the customer entered a quantity
+                                } else {
+                                    // run updateInventory() function
+                                    updateInventory(item.customerChoice, quantity.customerQuantity);
+                                }
+                            })
+                    }
+                }
+            )
+
         })
 }
 
 // function to update the "products" table within bamazon_db
 function updateInventory(item_wanted, quantity_wanted) {
     // connect to the database to do a request
-    connection.query (
+    connection.query(
         // grab the quantity in stock and the name of the product for the item wanted
         "SELECT stock_quantity, product_name FROM products WHERE ?",
         {
             item_id: parseInt(item_wanted),
         },
+        // then run the following
         function (err, res) {
             // if error(s), display it(them)
             if (err) throw err;
             // if the quantity wanted is superior to the quantity in stock
             if (quantity_wanted > res[0].stock_quantity) {
                 // display message - stocks too low
-                console.log(tooLowMsg.bold("-----------------------------------"));
-                console.log(tooLowMsg.bold("Sorry, we don't have that many in stock!"));
-                console.log(tooLowMsg.bold("-----------------------------------"));
-                // show the updated inventory and ask the customer again
+                console.log(warningMsg.bold("-----------------------------------"));
+                console.log(warningMsg.bold("Sorry, we don't have that many in stock!"));
+                console.log(warningMsg.bold("-----------------------------------"));
+                // show the inventory and ask the customer again if she/he wants to buy something
                 showInventoryAskCustomer();
-            // if the quantity in stock is enough
+                // if the quantity in stock is enough
             } else {
                 // display message  - successful transaction
                 console.log(sucessMsg.bold("-----------------------------------"));
@@ -172,15 +202,16 @@ function updateInventory(item_wanted, quantity_wanted) {
                             item_id: item_wanted
                         }
                     ],
-                    function(err, res) {
+                    // then run the following
+                    function (err, res) {
                         // if error(s), display it(them)
                         if (err) throw err;
                     }
                 )
-                // show the updated inventory and ask the customer again
+                // show the updated inventory and ask the customer again if she/he wants to buy something
                 showInventoryAskCustomer();
             }
-        }   
+        }
     )
 }
 
@@ -194,12 +225,12 @@ console.log(clc.magenta.bold("-----------------------------------"));
 console.log(clc.magenta.bold("Welcome on Bamazon - Outdoors Edition! Here is our inventory!"));
 console.log(clc.magenta.bold("-----------------------------------"));
 
-// open the connection to bamazon_db
-connection.connect(function(err) {
+// open the connection to bamazon_db and run the following
+connection.connect(function (err) {
     // if error(s), display it(them)
     if (err) throw err;
     // verify that we are connected
     // console.log("connected as id " + connection.threadId);
     // run the showInventoryAskCustomer() funcion once the connection has been established
-    showInventoryAskCustomer(); 
+    showInventoryAskCustomer();
 });
