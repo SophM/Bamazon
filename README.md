2f55b5d9e9c9d61072250d8a923ea2db96c4cd64
SophieM <47410186+SophM@users.noreply.github.com>
2019-03-25 15:16:21 -0700

    add updated README.md - with code snippet showing an example of input validation (for the first question)

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ In this project, I built an e-commerce storefront, called Bamazon. Bamazon is a 
 The app shows the store's inventory and takes orders from customers. The customer is asked to enter the ID of the item she/he wants to buy (first question), and then the quantity she/he wants (second question). If the quantity wanted by the customer is lower than the stock, the customer will "buy" the item(s) wanted and an updated store's inventory will be displayed. If the customer wants more items than available in stock, a "stock-too-low" message will be displayed along with the store's inventory. At anytime, the customer can quit the app by entering "Q". Some precautions have been taken so that entering "q" also quits Bamazon. 
 
 Input validations have been defined so that:
-- for the first question (which item?), if the customer enters a letter other than "Q"/"q" or a number that is not a current valid ID, a "not-a-valid-id" message will be displayed and the first question will be asked again.
+- for the first question (which item?), if the customer enters a letter other than "Q"/"q" or a number that is not a current valid ID, a "not-a-valid-id" message will be displayed and the first question will be asked again - see code snippet below for details.
 - for the second question (how many?), if the customer enters a letter other than "Q"/"q", a "not-a-valid-quantity" message will be displayed and the second question is asked again.
 
 The inventory is shown in a table format, thanks to the "cli-table" node package. Some colors have also been added to the inventory table and messages displayed thanks to the "cli-color" node package.
@@ -37,6 +37,12 @@ To build this project, I used the following technologies:
 - Node.js
 - JavaScript/jQuery
 - mySQL
+
+---
+
+### Code snippet showing the input validation for the first question (which item?)
+
+![Code snippet showing the input validation for the first question (which item?)](https://github.com/SophM/Bamazon/blob/master/assets/for_readme/screenshot_showing_validation_first_question.png?raw=true)
 
 ---
 
---
+++ bamazonManager.js
@@ -0,0 +1,241 @@
+// ------------------------------------------------------------------
+// Load the packages and files needed for the program to run
+// ------------------------------------------------------------------
+
+// load the inquirer node package to be able to get user inputs
+var inquirer = require("inquirer");
+
+// load the mysql node package to be able to connect to the Bamazon SQL database
+var mysql = require("mysql");
+
+// load the cli-color node package to be able to change the color
+// of the text displayed in the terminal
+var clc = require("cli-color");
+
+// load the cli-table node package to be able to display table in the terminal
+var table = require("cli-table");
+
+
+// ------------------------------------------------------------------
+// Connection to the mySQL database - bamazon_db
+// ------------------------------------------------------------------
+
+// create the connection to bamazon_db and store it in a variable
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // my port
+    port: 3306,
+
+    // my username
+    user: "root",
+
+    // my password
+    password: "a9uLc2frfg7buEidFA",
+    database: "bamazon_db"
+});
+
+
+// ------------------------------------------------------------------
+// Variables - color for styling
+// ------------------------------------------------------------------
+
+// create variable to store the option for the "manager" menu
+var menu = ["View products for sale", "View low inventory", "Add to inventory", "Add new product"];
+
+// create a color to display the "goodbye" message with cli-color
+var goodbyeMsg = clc.xterm(208);
+
+// create a color to display "warning" messages when too low quantity or not valid id
+// and "successful-transaction" message
+var sucessMsg = clc.xterm(71);
+var warningMsg = clc.xterm(160);
+
+
+// ------------------------------------------------------------------
+// Functions
+// ------------------------------------------------------------------
+
+// function to format nicely the "products" table and add some colors
+function showInventory(arr) {
+    // instantiate the table
+    var productsTable = new table();
+    // add the first row = headers - with some styling thanks to cli-color
+    productsTable.push([clc.cyan.bold("item_id"), clc.cyan.bold("product_name"), clc.cyan.bold("department_name"), clc.cyan.bold("price"), clc.cyan.bold("stock_quantity")]);
+    // add every item to the table
+    for (var i = 0; i < arr.length; i++) {
+        productsTable.push(
+            [arr[i].item_id, arr[i].product_name, arr[i].department_name, arr[i].price, arr[i].stock_quantity],
+        )
+    }
+    // display the table
+    console.log(productsTable.toString());
+}
+
+// function to diplay the "products" table to the manager = the inventory = products on sale
+function showInventoryAskManager() {
+    // connect to the database to do a request
+    connection.query(
+        // select the entire "products" table
+        "SELECT * FROM products",
+        // then run the following
+        function (err, res) {
+            // if error(s), display it(them)
+            if (err) throw err;
+            // show the "products" table in a nice table
+            showInventory(res);
+            // ask the manager what she/he would like to do
+            managerOptions();
+        }
+    )
+}
+
+// function to present the option to the manager 
+function managerOptions() {
+    // ask the customer to enter the ID of the item she/he wants
+    inquirer
+        .prompt([
+            {
+                type: "rawlist",
+                message: "What would you like to do? [Quit with Q]",
+                choices: menu,
+                name: "managerChoice"
+            }
+        // then run the following
+        ]).then(function (answer) {
+            // if the customer entered "Q" or "q"
+            if (item.customerChoice.toLowerCase() === "q") {
+               // display a "goodbye" message
+               console.log(goodbyeMsg.bold("-----------------------------------"));
+               console.log(goodbyeMsg.bold("Goodbye, see you next time!"));
+               console.log(goodbyeMsg.bold("-----------------------------------"));
+               // end the connection with bamazon_db
+               connection.end();             
+            // if the manager chose "View products on sale"
+            } else if (answer.managerChoice === menu[0]) {
+                // display message - not valid ID
+                console.log(clc.magenta.bold("-----------------------------------"));
+                console.log(clc.magenta.bold("Here is the inventory"));
+                console.log(clc.magenta.bold("-----------------------------------"));
+                // show the inventory and ask the manager again what she/he would like to do
+                showInventoryAskManager();
+            // if the manager chose "View low inventory"
+            } else if (answer.managerChoice === menu[1]) {
+
+            // if the manager chose "Add to inventory"    
+            } else if (answer.managerChoice === menu[2]) {
+
+            // if the manager chose "Add new product"      
+            } else if (answer.managerChoice === menu[3]) {
+                
+            }       
+        })
+}
+
+// function to ask the customer how many items does she/he want
+function askCustomerQuantity(customerChoice) {
+    // ask for how many she/he wants
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "How many would you like to buy? [Quit with Q]",
+                name: "customerQuantity"
+            }
+            // then run the following
+        ]).then(function (quantity) {
+            // if the customer entered a quantity, so a number - need to use "==" because the user input is a sting by default
+            if (quantity.customerQuantity == parseInt(quantity.customerQuantity)) {
+                // run updateInventory() function
+                updateInventory(customerChoice, quantity.customerQuantity);
+                // if the customer entered "Q" or "q"
+            } else if (quantity.customerQuantity.toLowerCase() === "q") {
+                // display a "goodbye" message
+                console.log(goodbyeMsg.bold("-----------------------------------"));
+                console.log(goodbyeMsg.bold("Goodbye, see you next time!"));
+                console.log(goodbyeMsg.bold("-----------------------------------"));
+                // end the connection with bamazon_db
+                connection.end();
+                // if the customer entered a letter other than "q" or "Q"
+            } else {
+                // display a message - not a valid quantity
+                console.log(warningMsg.bold("-----------------------------------"));
+                console.log(warningMsg.bold("I don't know how many is that! Please enter a valid quantity."));
+                console.log(warningMsg.bold("-----------------------------------"));
+                // ask the customer again how many does she/he wants
+                askCustomerQuantity(customerChoice);
+            }
+        })
+}
+
+// function to update the "products" table within bamazon_db
+function updateInventory(item_wanted, quantity_wanted) {
+    // connect to the database to do a request
+    connection.query(
+        // grab the quantity in stock and the name of the product for the item wanted
+        "SELECT stock_quantity, product_name FROM products WHERE ?",
+        {
+            item_id: parseInt(item_wanted),
+        },
+        // then run the following
+        function (err, res) {
+            // if error(s), display it(them)
+            if (err) throw err;
+            // if the quantity wanted is superior to the quantity in stock
+            if (quantity_wanted > res[0].stock_quantity) {
+                // display message - stocks too low
+                console.log(warningMsg.bold("-----------------------------------"));
+                console.log(warningMsg.bold("Sorry, we don't have that many in stock!"));
+                console.log(warningMsg.bold("-----------------------------------"));
+                // show the inventory and ask the customer again if she/he wants to buy something
+                showInventoryAskCustomerItem();
+                // if the quantity in stock is enough
+            } else {
+                // display message  - successful transaction
+                console.log(sucessMsg.bold("-----------------------------------"));
+                console.log(sucessMsg.bold("Successful transaction! You just purchased " + quantity_wanted + " " + res[0].product_name + "(s)."));
+                console.log(sucessMsg.bold("-----------------------------------"));
+                // connect to the database to do a request
+                connection.query(
+                    // update the quantity in stock for the item wanted
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: res[0].stock_quantity - parseInt(quantity_wanted)
+                        },
+                        {
+                            item_id: item_wanted
+                        }
+                    ],
+                    // then run the following
+                    function (err, res) {
+                        // if error(s), display it(them)
+                        if (err) throw err;
+                    }
+                )
+                // show the updated inventory and ask the customer which item she/he wants to buy
+                showInventoryAskCustomerItem();
+            }
+        }
+    )
+}
+
+
+// ------------------------------------------------------------------
+// Main process
+// ------------------------------------------------------------------
+
+// welcome the user - message displayed in color with cli-color
+console.log(clc.magenta.bold("-----------------------------------"));
+console.log(clc.magenta.bold("Welcome in the Bamazon manager's space!"));
+console.log(clc.magenta.bold("-----------------------------------"));
+
+// open the connection to bamazon_db and run the following
+connection.connect(function (err) {
+    // if error(s), display it(them)
+    if (err) throw err;
+    // verify that we are connected
+    // console.log("connected as id " + connection.threadId);
+    // run the managerOptions() funcion once the connection has been established
+    managerOptions();
+});
